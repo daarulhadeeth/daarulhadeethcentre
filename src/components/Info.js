2dab01050d6a83cd25c7935f3c4775c4db7646dc
abdulhamiid <adioabdulhamid@yahoo.com>
2024-05-28 09:45:40 +0100

    Add footer links

--- src/components/Info.js
+++ src/components/Info.js
@@ -29,7 +29,7 @@ function Showcase() {
               Center
             </h1>
             <h2 className="text-lg text-center">
-              for Islamic and technical studies
+              for Islamic and Technical Studies
             </h2>
             <br />
           </SlideIn>
